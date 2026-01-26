40a19e53b0fb06c67d01090f96be13a43a92936d
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2026-01-26 14:28:51 +0530

    add addExpense function and savestorage

--- expense-Tracker/src/main.js
+++ expense-Tracker/src/main.js
@@ -2,9 +2,13 @@ const expenseContainer = document.querySelector(".expense-container");
 let budgetText = document.querySelector("#budget");
 const categorySelect = document.querySelector("#byPrice");
 const addExpBtn = document.querySelector("#addExpBtn")
+const expenseNameInput = document.querySelector("#expName");
+const expenseAmountInput = document.querySelector("#expAmount");
+const expenseCategoryInput = document.querySelector("#expcategory");
+const expenseDateInput = document.querySelector("#expDate");
 
 
-const expenseArr = JSON.parse(localStorage.getItem("expense")) || [];
+const expenseArr = JSON.parse(localStorage.getItem("expenses")) || [];
 
 function createExpenseNode(expense) {
     const card = document.createElement("div");
@@ -19,10 +23,10 @@ function createExpenseNode(expense) {
                     <input class="expense-category-input" hidden />
                 </div>
                 <div class="right-text-area flex">
-                    <p>- ${expense.amount}</p>
+                    <p>- $${expense.amount}</p>
                     <p>${expense.date}</p>
                 </div>
-                <div>
+                <div class="btnArea flex" >
                 <button data-action="toggle">${expense.isDone ? "Undo" : "Done"}</button>
                 <button data-action="edit">Edit</button>
                 <button data-action="save" hidden>Save</button>
@@ -42,4 +46,37 @@ function initialRender() {
 
     expenseContainer.appendChild(fragment)
 }
+function saveToStorage() {
+    localStorage.setItem("expenses", JSON.stringify(expenseArr));
+}
+
+function addExpense() {
+    const expName = expenseNameInput.value;
+    const expCatg = expenseCategoryInput.value;
+    const expAmount = expenseAmountInput.value;
+    const expDate = expenseDateInput.value;
+
+    if(!expAmount || !expName || !expCatg || !expDate) return;
+
+    const expense = {
+        name : expName,
+        date : expDate,
+        category : expCatg,
+        amount : expAmount,
+        isDone : false,
+        id : Date.now()
+    }
+
+    expenseArr.push(expense);
+    saveToStorage();
+
+    expenseContainer.appendChild(createExpenseNode(expense));
+
+    expenseAmountInput.value = "";
+    expenseNameInput.value = "";
+    expenseDateInput.value ="";
+    expenseCategoryInput.selectIndex = 0;
+
+}
+addExpBtn.addEventListener("click",addExpense);
 initialRender();
